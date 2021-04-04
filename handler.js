a43b3a06fb80bdbc88fe32a574df2de3eea7b73f
Rodrigo <r@slino.com.br>
2021-04-03 21:23:13 -0300

    start

---
+++ handler.js
@@ -0,0 +1,30 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const auth = require("./auth");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/hello1", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.get("/hello3", auth, (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = serverless(app);
